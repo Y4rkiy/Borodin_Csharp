bec456ef6f529bb0ad682cb0db341a2ed1bb9ad1
Y4rkiy <yaroslav.borodin@nure.ua>
2021-11-12 11:07:37 +0200

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //first algorithm
+            Console.WriteLine("Введите число");
+            int inputInt = Convert.ToInt32(Console.ReadLine());
+            if (inputInt == 7)
+            {
+                Console.WriteLine("Привет");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //second algorithm
+            Console.WriteLine("Введите имя");
+            string inputString = Console.ReadLine();
+            if (inputString == "Вячеслав")
+            {
+                Console.WriteLine("Привет, Вячеслав");
+            }
+            else
+            {
+                Console.WriteLine("Нет такого имени");
+            }
+            Console.WriteLine();
+
+            //third algorithm
+            Console.WriteLine("Введите числа через пробел");
+            string inputArr = Console.ReadLine();
+            int[] arr = inputArr.Split(' ').Select(x => int.Parse(x)).ToArray();
+            foreach (int a in arr)
+            {
+                if (a % 3 == 0)
+                {
+                    Console.Write($"{a} ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        
+    }
+}
